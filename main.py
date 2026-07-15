596db2c842808eb3c4cf89a52c24075803eeea39
eddymatos2309-bit <eddymatos2309@gmail.com>
2026-07-15 01:40:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from binance.client import Client
 # 1. CONFIGURACIÓN DE CREDENCIALES (Cargadas de forma segura desde la nube)
 client = Client('', '')  # No requiere llaves para leer datos públicos
 
-TELEGRAM_TOKEN = os.environ.get('8968451696:AAF_QGs61ZQLDGVmjhLsP_2GoK1J3mDFcA8')
-TELEGRAM_CHAT_ID = os.environ.get('8737478796')
+TELEGRAM_TOKEN = '8968451696:AAF_QGs61ZQLDGVmjhLsP_2GoK1J3mDFcA8'
+TELEGRAM_CHAT_ID = '8737478796'
 
 # 2. PARÁMETROS DEL RADAR
 LIMITE_1H = 0.05   
